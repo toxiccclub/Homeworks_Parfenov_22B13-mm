ed707f5102feff59f29ebae77ad2caa3f0f0e200
toxiccclub <150171690+toxiccclub@users.noreply.github.com>
2024-02-23 21:13:46 +0300

    Update 9.cpp

--- 17.02.2024/9.cpp
+++ 17.02.2024/9.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 int main() {
 	vector<double> mas = { 1,2,3,3,3,4,4,5,5,5,5,9,9,9,10,10 };
-	vector<double>::iterator it;
-	it = upper_bound(mas.begin(), mas.end(), 5);
-	cout << it - mas.begin() - 1;
+	vector<double>::iterator ita,itb;
+	ita = upper_bound(mas.begin(), mas.end(), 5);
+	itb = lower_bound(mas.begin(), mas.end(), 5);
+	if (itb != ita) {
+		cout << ita - mas.begin() - 1;
+	}
+	else {
+		cout << "Error" << endl;
+	}
 	return 0;
 }
